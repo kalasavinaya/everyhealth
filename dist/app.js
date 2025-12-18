fe5b90737fbd206a7020cc10ada68a3c3217daf0
Vinaya <vk.kalasa@gmail.com>
2025-12-18 15:56:13 +0530

    Add backend and frontend folders

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import logRouter from "./routes/logs.js";
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/logs", logRouter);
-export default app;
-//# sourceMappingURL=app.js.map
--- dist/controllers/logsController.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Request, Response } from "express";
-export declare function getLogs(req: Request, res: Response): Promise<void>;
-export declare function uploadLogs(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=logsController.d.ts.map
