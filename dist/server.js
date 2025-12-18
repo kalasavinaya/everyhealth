7fc2a679695ab590c47aa09d75b9ef7fe8326f09
Vinaya <vk.kalasa@gmail.com>
2025-12-18 15:50:55 +0530

    Remove dist folders from Git and update gitignore

--- dist/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server:port:${PORT}`));
-//app.listen(PORT, ()=>console.log('Server---:port: ${PORT}'));
-//# sourceMappingURL=server.js.map
